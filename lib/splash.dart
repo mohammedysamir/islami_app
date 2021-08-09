37221975ef5ec3ca58c86265b50688adee1bae3f
Mohammed Yasser <mohammedre4a@gmail.com>
2021-08-09 15:04:33 +0200

    Add: create most of the splash UI. **Note: need to increase width of the background.

---
+++ lib/splash.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   String routeName="Splash";
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: "Splash screen",
+//         home: Scaffold(
+//          body:Stack(
+//            children: [
+//              Container(child: Image.asset("assets/images/bg2.png")),
+//              Center(
+//                child: Column(
+//                  mainAxisAlignment: MainAxisAlignment.center,
+//                  children: [
+//                    Image.asset("assets/images/logo2.png"),
+//                    Image.asset("assets/images/route gold.png")
+//                  ]
+//                ),
+//              )
+//            ],
+//          )
+//         ),
+//     );
+//   }
+// }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Splash(),
+    );
+  }
+}
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        Container(
+          child: Image.asset("assets/images/bg2.png"),
+          width: MediaQuery.of(context).size.width,
+        ),
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(8.0, 200.0, 8.0, 0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset("assets/images/logo2.png"),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8.0, 50.0, 8.0, 0),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset("assets/images/route gold.png"),
+                          Text(
+                            "supervised by Mohamed Nabil",
+                            style: TextStyle(
+                              color: Color.fromARGB(200, 183, 147, 95),
+                              fontSize: 18,
+                            ),
+                          )
+                        ]),
+                  )
+                ]),
+          ),
+        )
+      ],
+    ));
+  }
+}
