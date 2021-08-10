7a6582ce3fd77d76b2e8a6c24f54fea9aea0638d
Mohammed Yasser <mohammedre4a@gmail.com>
2021-08-10 18:39:48 +0200

    try to merge with development branch

--- lib/splash.dart
+++ lib/splash.dart
@@ -12,7 +12,7 @@ class SplashWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    initialRoute: 'Splash',
+      initialRoute: 'Splash',
       routes: {
         'Splash': (context) => const Splash(),
         '/quran': (context) => const QuranScreen()
@@ -42,44 +42,47 @@ class _SplashState extends State<Splash> {
   void initState() {
     super.initState();
     Timer(Duration(seconds: 4),
-        () => Navigator.pushReplacement(context, route()));
+            () => Navigator.pushReplacement(context, route()));
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
         body: Stack(
-      children: [
-        Container(
-          child: Image.asset("assets/images/bg2.png", fit: BoxFit.fill),
-          width: MediaQuery.of(context).size.width,
-        ),
-        Center(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(8.0, 200.0, 8.0, 0),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset("assets/images/logo2.png"),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(8.0, 50.0, 8.0, 0),
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset("assets/images/route gold.png"),
-                          Text(
-                            "supervised by Mohamed Nabil",
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 183, 147, 95),
-                              fontSize: 18,
-                            ),
-                          )
-                        ]),
-                  )
-                ]),
-          ),
-        ),
-      ],
-    ));
+          children: [
+            Container(
+              child: Image.asset("assets/images/bg2.png", fit: BoxFit.fill),
+              width: MediaQuery
+                  .of(context)
+                  .size
+                  .width,
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(8.0, 200.0, 8.0, 0),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/images/logo2.png"),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(8.0, 50.0, 8.0, 0),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset("assets/images/route gold.png"),
+                              Text(
+                                "supervised by Mohamed Nabil",
+                                style: TextStyle(
+                                  color: Color.fromARGB(255, 183, 147, 95),
+                                  fontSize: 18,
+                                ),
+                              )
+                            ]),
+                      )
+                    ]),
+              ),
+            ),
+          ],
+        ));
   }
 }
