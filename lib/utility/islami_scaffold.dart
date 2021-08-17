f9e5ef5a454d6f19143aab876bbf351c27e33cb6
Mohammed Yasser <mohammedre4a@gmail.com>
2021-08-17 22:03:42 +0200

    Add: translate the appBar title to arabic.

--- lib/utility/islami_scaffold.dart
+++ lib/utility/islami_scaffold.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../BottomNavBar.dart';
 
 class IslamiScaffold extends StatelessWidget {
@@ -28,7 +28,7 @@ class IslamiScaffold extends StatelessWidget {
       ),
       child: Scaffold(
         appBar: AppBar(
-          title:  Text("إسلامي",),
+          title:  Text(AppLocalizations.of(context)!.eslami,),
           centerTitle: true,
           elevation: 0,
         ),
